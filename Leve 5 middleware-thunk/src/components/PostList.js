b2ca690fcd03db8ca528c4fa34192ba823db0133
minh997 <39399443+minh997@users.noreply.github.com>
2019-05-16 21:06:04 -0700

    Fetch post data and display it

--- Leve 5 middleware-thunk/src/components/PostList.js
+++ Leve 5 middleware-thunk/src/components/PostList.js
@@ -5,17 +5,38 @@ import { fetchPosts } from "../actions";
 class PostList extends Component {
     //fetch data, store in reducer
     componentDidMount() {
-        console.log(this.state);
+        //class doennt hold any state, all state is stored in combineReducer
         this.props.fetchPosts();
     }
 
-    render() {
+    renderList() {
         console.log(this.props);
-        return <div>PostList</div>;
+
+        return this.props.posts.map(post => {
+            return (
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user" />
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                </div>
+            );
+        });
+    }
+
+    render() {
+        return (
+            <div className="ui relaxed divided list">{this.renderList()}</div>
+        );
     }
 }
 
 //this component will have 1 props is posts:...
+
+//get state from reducer, convert it to this component props
 const mapStateToProps = state => {
     return {
         posts: state.posts
